6871a3636964cccbca92890d12f9aff10583cc57
byunghyunpark <openmind8735@gmail.com>
2016-10-31 21:45:28 +0900

    Move photo views to the views dir.

--- django_app/photo/views/photo.py
+++ django_app/photo/views/photo.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
+
+__all__ = [
+    'photo_list',
+]
+
 def photo_list(request):
     return render(request, 'photo/photo_list.html', {})
