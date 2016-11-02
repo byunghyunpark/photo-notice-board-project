4922e713a1c425770dace4c15f1647ea9fb1c603
byunghyunpark <openmind8735@gmail.com>
2016-11-02 17:24:59 +0900

    Add Photo model.

--- django_app/photo/models.py
+++ django_app/photo/models.py
@@ -11,3 +11,11 @@ class Album(BaseModel):
 
     def __str__(self):
         return self.title
+
+
+class Photo(BaseModel):
+    album = models.ForeignKey(Album)
+    title = models.CharField(max_length=30)
+    description = models.TextField(blank=True)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL)
+    img = models.ImageField(upload_to='photo')
