61113fc40576d53637521a03ac708c9d59b83d2b
byunghyunpark <openmind8735@gmail.com>
2016-11-02 18:12:27 +0900

    Add feature: show img in photo list.

--- django_app/photo/views/photo.py
+++ django_app/photo/views/photo.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
+from ..models import Photo
 
 __all__ = [
     'photo_list',
 ]
 
 def photo_list(request):
-    return render(request, 'photo/photo_list.html', {})
+    all_photo = Photo.objects.all().order_by('-created_date')
+
+    context = {
+        'all_photo': all_photo,
+    }
+
+    return render(request, 'photo/photo_list.html', context)
