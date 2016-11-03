c163e8721a8579764cd95c6ee53fb68597e320ba
byunghyunpark <openmind8735@gmail.com>
2016-11-03 15:15:14 +0900

    Add model: PhotoLike and PhotoDislike for M2M.

--- django_app/photo/models.py
+++ django_app/photo/models.py
@@ -19,6 +19,18 @@ class Photo(BaseModel):
     description = models.TextField(blank=True)
     owner = models.ForeignKey(settings.AUTH_USER_MODEL)
     img = models.ImageField(upload_to='photo')
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, through='PhotoLike', related_name='photo_set_like_users')
+    dislike_users = models.ManyToManyField(settings.AUTH_USER_MODEL, through='PhotoDislike', related_name='photo_set_dislike_users')
 
     def __str__(self):
         return self.title
+
+
+class PhotoLike(BaseModel):
+    photo = models.ForeignKey(Photo)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+
+
+class PhotoDislike(BaseModel):
+    photo = models.ForeignKey(Photo)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
